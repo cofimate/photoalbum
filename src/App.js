6a784b7da09855f5b8f210bb1adfb353206d20cd
Cecilio Cofler <ccofler@gmail.com>
2021-04-01 18:27:53 -0700

    Adding changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React , {Component } from 'react';
-import logo from './logo.svg';
+
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 // function App() {
@@ -28,28 +29,27 @@ class App extends Component {
       super();
 
       this.state = {
-        monsters : [
-          {
-              id : '1',
-              name : 'Frankenstein',
-          },
-          {
-              id : '2',
-              name : 'Dracula',
-          },
-          {
-            id : '3',
-            name : 'Zombie',
-          }
-        ]
+        monsters : [],
+        searchField: ''
       }
   }
+
+  componentDidMount() { 
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users =>  this.setState( {  monsters : users }));
+  }
+
   render() {
+    const  { monsters , searchField } =  this.state;
+    const filteredMonsters  = monsters.filter( monster => 
+       monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
         <div className="App">
-          {
-            this.state.monsters.map( monster => <h1 key={monster.id}>{monster.name}</h1>)
-          }
+          <input type='string' placeholder='search monsters'
+             onChange={  e => this.setState({ searchField : e.target.value}) } />
+          <CardList monsters={filteredMonsters} />
         </div>
       ) 
   }
